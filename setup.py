7d6bd16327c7f39c6e409f290535434dc05d0a88
Kaustubh Badrike <10396572-kaustubhbadrike@users.noreply.gitlab.com>
2022-06-21 15:24:28 -0400

    Sort deps

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ requirements = [
     "artiq @ git+https://github.com/m-labs/artiq.git",
     "dax @ git+https://gitlab.com/duke-artiq/dax@v6.8",
     "dax-program-sim @ git+https://gitlab.com/duke-artiq/dax-program-sim.git",
+    "Jinja2>=2.11.3",
     "qiskit>=0.36.2",
     "sipyco @ git+https://github.com/m-labs/sipyco@v1.4",
     "toml>=0.10.2",
-    "Jinja2>=2.11.3",
 ]
 
 
