adf491b3264f658aebb399b0863b341429de5311
Kaustubh Badrike <10396572-kaustubhbadrike@users.noreply.gitlab.com>
2022-09-29 11:09:45 -0400

    generalized dj

--- tests/test_circuit_dj.py
+++ tests/test_circuit_dj.py
@@ -1,38 +1,78 @@
 from qiskit import *
 from qiskit.providers.dax import DAX
+import numpy as np
 dax = DAX.get_provider() # aqt is a provider
 
 backend = dax.get_backend('dax_code_simulator') 
 # backend = dax.get_backend('dax_code_printer') 
 
-# region Basic
-q= QuantumRegister(4)
-c= ClassicalRegister(4)
-qc = QuantumCircuit(q, c)
-# endregion
+def dj_oracle(case, n):
+    # We need to make a QuantumCircuit object to return
+    # This circuit has n+1 qubits: the size of the input,
+    # plus one output qubit
+    oracle_qc = QuantumCircuit(n+1)
+    
+    # First, let's deal with the case in which oracle is balanced
+    if case == "balanced":
+        # First generate a random number that tells us which CNOTs to
+        # wrap in X-gates:
+        b = np.random.randint(1,2**n)
+        # Next, format 'b' as a binary string of length 'n', padded with zeros:
+        b_str = format(b, '0'+str(n)+'b')
+        # Next, we place the first X-gates. Each digit in our binary string 
+        # corresponds to a qubit, if the digit is 0, we do nothing, if it's 1
+        # we apply an X-gate to that qubit:
+        for qubit in range(len(b_str)):
+            if b_str[qubit] == '1':
+                oracle_qc.x(qubit)
+        # Do the controlled-NOT gates for each qubit, using the output qubit 
+        # as the target:
+        for qubit in range(n):
+            oracle_qc.cx(qubit, n)
+        # Next, place the final X-gates
+        for qubit in range(len(b_str)):
+            if b_str[qubit] == '1':
+                oracle_qc.x(qubit)
 
-# region DJ algo
-qc.h(q[0])
-qc.x(q[0])
-qc.h(q[1])
-qc.h(q[2])
-qc.x(q[2])
-qc.x(q[3])
-qc.h(q[3])
-qc.cx(q[0], q[3])
-qc.cx(q[1], q[3])
-qc.cx(q[2], q[3])
-qc.x(q[0])
-qc.x(q[2])
-qc.h(q[0])
-qc.h(q[1])
-qc.h(q[2])
+    # Case in which oracle is constant
+    if case == "constant":
+        # First decide what the fixed output of the oracle will be
+        # (either always 0 or always 1)
+        output = np.random.randint(2)
+        if output == 1:
+            oracle_qc.x(n)
+    
+    oracle_gate = oracle_qc.to_gate()
+    oracle_gate.name = "Oracle" # To show when we display the circuit
+    return oracle_gate
 
-# endregion
-qc.measure_all()
+def dj_algorithm(oracle, n):
+    dj_circuit = QuantumCircuit(n+1, n)
+    # Set up the output qubit:
+    dj_circuit.x(n)
+    dj_circuit.h(n)
+    # And set up the input register:
+    for qubit in range(n):
+        dj_circuit.h(qubit)
+    # Let's append the oracle gate to our circuit:
+    dj_circuit.append(oracle, range(n+1))
+    # Finally, perform the H-gates again and measure:
+    for qubit in range(n):
+        dj_circuit.h(qubit)
+    
+    for i in range(n):
+        dj_circuit.measure(i, i)
+    
+    return dj_circuit
+
+n = 3
+oracle_gate = dj_oracle('balanced', n)
+dj_circuit = dj_algorithm(oracle_gate, n)
+
+
 
 backend.load_config("resources.toml")
-dax_job = execute(qc, backend, shots=30, optimization_level=0)
+dax_job = execute(dj_circuit, backend, shots=30, optimization_level=0)
 res = dax_job.result()
 counts = res.get_counts()
 print(counts, res.to_dict())
