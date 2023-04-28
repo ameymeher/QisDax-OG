c259487df1942a95de4100d9c7db29b487a81aec
Kaustubh Badrike <10396572-kaustubhbadrike@users.noreply.gitlab.com>
2023-04-28 11:17:28 -0400

    cryo s

--- tests/automate.sh
+++ tests/automate.sh
@@ -6,24 +6,24 @@ pyenv='/home/kjbadrik/anaconda3/envs/qisdax/bin/python3'
 for i in $(seq $count); do
     $pyenv bv.py
 done
-mv profile.txt bv.cryo.parallel.txt
+mv profile.txt bv.cryo.serial.txt
 
 for i in $(seq $count); do
     $pyenv dj.py
 done
-mv profile.txt dj.cryo.parallel.txt
+mv profile.txt dj.cryo.serial.txt
 
 for i in $(seq $count); do
     $pyenv ghz.py
 done
-mv profile.txt ghz.cryo.parallel.txt
+mv profile.txt ghz.cryo.serial.txt
 
 for i in $(seq $count); do
     $pyenv grover_simpl.py
 done
-mv profile.txt grover.cryo.parallel.txt
+mv profile.txt grover.cryo.serial.txt
 
 for i in $(seq $count); do
     $pyenv simon.py
 done
-mv profile.txt simon.cryo.parallel.txt
+mv profile.txt simon.cryo.serial.txt
