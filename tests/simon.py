3e166201022033771552e0da694fe4243803fee3
Kaustubh Badrike <10396572-kaustubhbadrike@users.noreply.gitlab.com>
2023-04-28 12:10:29 -0400

    Instructions

--- tests/simon.py
+++ tests/simon.py
@@ -62,8 +62,8 @@ def bdotz(b, z):
 
 dax = DAX.get_provider() # aqt is a provider
 
-# backend = dax.get_backend('dax_code_simulator') 
-backend = dax.get_backend('dax_code_printer') 
+backend = dax.get_backend('dax_code_simulator') 
+# backend = dax.get_backend('dax_code_printer') 
 backend.load_config("resources.toml")
 with open('profile.txt', 'a') as f:
     f.write(str(process_time_ns()))
