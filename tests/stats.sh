e063111175dab91931d617063efa350be151a990
Kaustubh Badrike <10396572-kaustubhbadrike@users.noreply.gitlab.com>
2023-05-01 10:46:13 -0400

    script

---
+++ tests/stats.sh
@@ -0,0 +1,31 @@
+git clone git@gitlab.com:fmuelle/qisdax.git
+
+cd qisdax
+
+conda env create -f environment.yml
+conda activate qisdax
+pip3 install -r requirements.txt
+
+git checkout profile
+
+cd tests
+
+git checkout aria-p
+./automate.sh && ./mw-automate.sh
+
+git checkout aria-s
+./automate.sh && ./mw-automate.sh
+
+git checkout cryo-s
+./mw-automate.sh
+
+git checkout cryo-p
+./mw-automate.sh
+
+git checkout profile
+python3 consolidate.py
+
+cp qisdax.json ../../qisdax.json
+cd ../..
+# Activate any environment with the `numpy`, `scipy` and `pandas` packages
+jupyter run qisdax_svp_rt.ipynb
